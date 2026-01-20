dc5b18fa9ed92d1924d553e58395ab125118c04d
Atharwa Dewasthali <classesetcxaviers@gmail.com>
2026-01-20 17:23:36 +0530

    Delete errors directory

--- errors/InternalServerError.js
+++
@@ -1,9 +0,0 @@
-import appError from "./appError.js";
-
-class InternalServerError extends appError {
-    constructor(error) {
-        super(error, 500);
-    }
-}
-
-export default InternalServerError;
