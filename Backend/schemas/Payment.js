866d2048d11987dcec233ccf0f164937a3c4f0a7
Sparky1445 <classesetcxaviers@gmail.com>
2026-01-27 11:27:24 +0530

    Added payment Schema

---
+++ Backend/schemas/Payment.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const paymentSchema = new mongoose.Schema({
+    bookingId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Booking',
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["pending", "success", "failed"],
+        default: "pending"
+    }
+}, {
+    timestamps: true
+})
+
+const Payment = mongoose.model("Payment", paymentSchema);
+export default Payment;
