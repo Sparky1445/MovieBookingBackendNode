b789ae86a89153c185819ae41636fd74723898bd
Aniket Paneri <aniketpaneri@gmail.com>
2026-02-02 01:02:31 +0530

    header chnaged

--- Frontend/movie-booking-system/src/Header.jsx
+++ Frontend/movie-booking-system/src/Header.jsx
@@ -1,7 +1,12 @@
-function Header(){
-    return(
+import profileLogo from "./assets/profile.png"
+function Header() {
+    return (
         <header className="header">
             <p className="header-text">CINEPLEX</p>
+            <div className="header-right">
+                <p className="header-SignIn">SignIn</p>
+                <img src={profileLogo} alt="Profile" className="profile-logo" />
+            </div>
         </header>
     );
 }
