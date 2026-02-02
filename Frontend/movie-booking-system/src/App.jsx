fb860f5207d1a4bd0735fdbe3eb7d1d4aa5df797
pgoltekar9 <149804273+pgoltekar9@users.noreply.github.com>
2026-02-03 00:07:52 +0530

    navbar

--- Frontend/movie-booking-system/src/App.jsx
+++ Frontend/movie-booking-system/src/App.jsx
@@ -1,8 +1,12 @@
 import Header from './Header.jsx'
+import Navbar from './Navbar.jsx';
 
 function App() {
   return(
+    <>
     <Header/>
+    <Navbar/>
+    </>
   );
 }
 
---
+++ Frontend/movie-booking-system/src/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar(){
+    return(
+        <nav>
+            <p className="nav"> HOME MOVIES EVENTS PLAYS SPORTS ACTIVITIES</p>
+        </nav>
+    );
+}
+
+export default Navbar
